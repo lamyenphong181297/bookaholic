af07cc729614e8dd4cfc88c46f9dadabee2eb9a8
Nguyen Kien <kiennd010498@gmail.com>
2020-04-04 18:06:21 +0700

    add bootstrap + example

--- src/App.js
+++ src/App.js
@@ -4,24 +4,36 @@ import BOOKS_DATA from './data/books.data';
 
 import './App.css';
 
+import ReviewModal from './components/review-modal/review-modal.component';
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       books: [],
-      searchField: ''
+      searchField: '',
+      show: false
     };
+    this.changeShowState = this.changeShowState.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ books: BOOKS_DATA.slice(0, 10) });
+    this.setState({ books: BOOKS_DATA.slice(0, 10)});
+  }
+
+  changeShowState() {
+    this.setState(prevState => ({
+      show: !prevState.show
+    }));
   }
 
   render() {
+    console.log(this.state.books[0]);
     return (
       <div className='App'>
         <h1>BOOKAHOLIC</h1>
+        <ReviewModal changeShowState={this.changeShowState} book={this.state.books[0]} show={this.state.show}/>
       </div>
     );
   }
